46ce06a3850e08c463578a957f19949e72da8c43
Rafal Woycicki <rafalwoycicki@users.noreply.github.com>
2025-04-22 09:54:58 +0200

    Update ddradseq_pre.bash

--- ddradseq_pre.bash
+++ ddradseq_pre.bash
@@ -28,7 +28,7 @@ C_p5_3p="P5read3primMSE1_DBR=TTAGCNNNNNNNN" # read P5 3prim cut site for the MSE
 C_p7_5p="P7read5primDBR_MSE1=^NNNNNNNNGCTAA" # read P7 5prim cut site for the MSE1 RE including DBR region
 C_p7_3p="P7read3primSBF1=CCTGCA" # read P7 3 prim cut site for the SBF1 RE
 
-echo "variable: $iftest $qualada $qualfil $errbar $errfil $errfilanc $thr $lenada $lenfil $readsP5 $readsP7 $barcodes $directory A_p5_5p A_p5_3p A_p7_5p A_p7_3p"
+echo "variable: $iftest $qualada $qualfil $errbar $errfil $errfilanc $thr $lenada $lenfil $readsP5 $readsP7 $barcodes $directory $A_p5_5p $A_p5_3p $A_p7_5p $A_p7_3p $C_p5_5p $C_p5_3p $C_p7_5p $C_p7_3p"
 
 cat $barcodes | grep '>' | sed 's/>//' > barcodes.list
 
